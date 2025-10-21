37cc7cff4a14826bc597add1dcdabb400b8d6668
Fabricio <a20214115@pucp.edu.pe>
2025-10-21 11:20:13 -0500

    feat: :sparkles: Add validations for max length and spaces

--- apps/frontend/src/modules/health/pages/specialities/models/speciality.ts
+++ apps/frontend/src/modules/health/pages/specialities/models/speciality.ts
@@ -1,7 +1,14 @@
 import { z } from 'zod'
 
 export const formSpecialitySchema = z.object({
-  name: z.string().nonempty('El nombre es obligatorio'),
+  name: z
+    .string()
+    .nonempty('El nombre es obligatorio')
+    .trim()
+    .refine((val) => val.trim().length > 0, {
+      message: 'El nombre no puede contener solo espacios',
+    })
+    .max(100, { message: 'El nombre no debe superar los 100 caracteres' }),
 })
 
 export type FormSpecialitySchema = z.infer<typeof formSpecialitySchema>
