102b95b595b861c8057d9f113381da77f64fc5dc
Naye <nayelymaria.poma@gmail.com>
2025-10-21 20:08:14 -0500

    chore(frontend): remove inline comment in edit-activity

--- apps/frontend/src/modules/health/pages/activities/pages/edit-activity-form/index.tsx
+++ apps/frontend/src/modules/health/pages/activities/pages/edit-activity-form/index.tsx
@@ -106,7 +106,6 @@ export default function EditActivityForm() {
             : [{ alliedId: '', specialityIds: [] }],
       })
 
-      // Refuerzo explícito para selects controlados
       setValue('typeId', activity.typeId)
       setValue('statusId', activity.statusId)
     }
