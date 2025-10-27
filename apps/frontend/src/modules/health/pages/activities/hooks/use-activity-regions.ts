da38dda5a0226398e89bb4784df7b91259777648
Naye <nayelymaria.poma@gmail.com>
2025-10-27 13:23:08 -0500

    fix(activity): querykeys

--- apps/frontend/src/modules/health/pages/activities/hooks/use-activity-regions.ts
+++ apps/frontend/src/modules/health/pages/activities/hooks/use-activity-regions.ts
@@ -1,9 +1,10 @@
 import rpc from '@frontend/lib/rpc'
+import { QueryKeys } from '@frontend/shared/constants/query-keys'
 import { useQuery } from '@tanstack/react-query'
 
 export const useActivityRegions = () => {
   return useQuery({
-    queryKey: ['health-activity-regions'],
+    queryKey: [QueryKeys.HEALTH.ACTIVITY_REGIONS],
     queryFn: async () => {
       const { data, error } = await rpc.health.activities.regions.get()
 
