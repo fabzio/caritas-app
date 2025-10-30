5da5d483431330f19dea0614f7c9145b030e7acd
Dalpb <dpalomino032002@gmail.com>
2025-10-30 13:28:15 -0500

    fix: black space

--- apps/frontend/src/modules/education/pages/fair/pages/create-fair/utils/fair.ts
+++ apps/frontend/src/modules/education/pages/fair/pages/create-fair/utils/fair.ts
@@ -4,6 +4,7 @@ export const formFairSchema = z
   .object({
     title: z
       .string()
+      .trim()
       .max(200, {
         message: 'El nombre de la feria no debe superar los 200 caracteres',
       })
@@ -11,10 +12,14 @@ export const formFairSchema = z
     date: z.date({ message: 'La fecha de la feria es obligatoria' }),
     address: z
       .string()
+      .trim()
       .nonempty('Ingrese la ubicación de la feria vocacional')
       .max(200, { message: 'La dirección no debe superar los 200 caracteres' }),
-    startTime: z.string().nonempty('Ingrese la hora de inicio de la feria'),
-    endTime: z.string().nonempty('Ingrese la hora de fin de la feria'),
+    startTime: z
+      .string()
+      .trim()
+      .nonempty('Ingrese la hora de inicio de la feria'),
+    endTime: z.string().trim().nonempty('Ingrese la hora de fin de la feria'),
     regionId: z.number().min(1, { message: 'Debes seleccionar una región' }),
   })
   .refine((data) => data.startTime < data.endTime, {
