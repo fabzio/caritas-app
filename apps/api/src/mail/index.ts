1f1f246a07d9ca63e4f3597d1c5edbecb24c2e93
DiegoIzaguirreLam <izaguirrediego12@gmail.com>
2025-11-18 19:17:36 -0500

    fix: undo insecure smtp transporter

--- apps/api/src/mail/index.ts
+++ apps/api/src/mail/index.ts
@@ -8,7 +8,7 @@ export const SENDER =
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST,
   port: env.SMTP_PORT,
-  secure: false,
+  requireTLS: true,
   auth: {
     user: env.SMTP_USER,
     pass: env.SMTP_PASSWORD,
