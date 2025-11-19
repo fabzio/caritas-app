ec57d67edd68e4841789043f212ecfcaae7e4dde
superflash41 <saymonnichorayo@gmail.com>
2025-11-19 15:55:17 -0500

    feat: added dashboard for education

--- apps/frontend/src/routes/_authenticated/education/index.tsx
+++ apps/frontend/src/routes/_authenticated/education/index.tsx
@@ -1,9 +1,6 @@
+import EducationDashboard from '@frontend/modules/education/pages/dashboard'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authenticated/education/')({
-  component: RouteComponent,
+  component: EducationDashboard,
 })
-
-function RouteComponent() {
-  return <div>Hello "/_authenticated/education/"!</div>
-}
