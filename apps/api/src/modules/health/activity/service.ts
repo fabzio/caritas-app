23d6876fadc78405aa77793ac980c837d76d38a4
CSupay <a20201921@pucp.edu.pe>
2025-11-19 23:42:36 -0500

    fix: document number enhanced, many fixes for insuranceType

--- apps/api/src/modules/health/activity/service.ts
+++ apps/api/src/modules/health/activity/service.ts
@@ -851,6 +851,38 @@ export const getRegionsWithActivities = async () => {
   }
 }
 
+export const getSingleUserActivity = async (
+  userId: string,
+): Promise<ActivityModel.SingleUserActivity | null> => {
+  const [userData] = await db
+    .select({
+      id: user.id,
+      name: user.name,
+      surname: user.surname,
+      documentType: user.documentType,
+      documentNumber: user.documentNumber,
+      email: user.email,
+      phone: user.phone,
+      birthDate: user.birthDate,
+      sex: user.sex,
+      regionId: user.regionId,
+      insuranceType: patientInfo.insuranceType,
+    })
+    .from(user)
+    .innerJoin(patientInfo, eq(user.id, patientInfo.userId))
+    .where(eq(user.id, userId))
+    .limit(1)
+
+  if (!userData) return null
+
+  return {
+    data: {
+      ...userData,
+      birthDate: new Date(userData.birthDate),
+    },
+  }
+}
+
 export const getExistentUsers = async (
   params: ActivityModel.ListExistentUsersQuery,
 ): Promise<ActivityModel.ExistentUser> => {
--- apps/frontend/src/modules/health/pages/activities/pages/add-attendant/hooks/use-edit-attendant.tsx
+++ apps/frontend/src/modules/health/pages/activities/pages/add-attendant/hooks/use-edit-attendant.tsx
@@ -11,6 +11,7 @@ type UpdateAttendantProps = Parameters<
 >[0] & {
   teamIds?: string[]
   activityId: number
+  insuranceType: 'none' | 'public' | 'private'
 }
 
 export const useUpdateAttendant = () => {
@@ -30,12 +31,26 @@ export const useUpdateAttendant = () => {
           .patch({
             teamIds,
           })
+      const { error: patientError } = await rpc.auth.info
+        .patient({ userId: props.userId as string })
+        .patch({
+          insuranceType: props.insuranceType,
+        })
+      if (patientError) throw patientError
       return data
     },
     onSuccess: (_, { userId, activityId }) => {
       queryClient.invalidateQueries({ queryKey: [QueryKeys.ADMIN.USERS] })
       queryClient.invalidateQueries({
         queryKey: [QueryKeys.ADMIN.USERS, userId],
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.HEALTH.ACTIVITIES],
+        refetchType: 'all',
+      })
+      queryClient.invalidateQueries({
+        queryKey: [QueryKeys.HEALTH.ADD_ATTENDANT],
+        refetchType: 'all',
       })
       if (session?.user.id === userId)
         queryClient.invalidateQueries({ queryKey: [QueryKeys.ACCESS] })
